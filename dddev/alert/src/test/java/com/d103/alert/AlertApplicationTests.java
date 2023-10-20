59ce4067d7783eca87ced7e942e511eecde7d955
최가연 <cn01803@naver.com>
2023-10-20 15:40:14 +0900

    설정: 커밋 알림 프로젝트 생성 및 설정

---
+++ dddev/alert/src/test/java/com/d103/alert/AlertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.d103.alert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
