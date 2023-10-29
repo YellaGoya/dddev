69f272a9e21ae131c2d8a9fc919ea0793bd03c0d
최가연 <cn01803@naver.com>
2023-10-29 21:56:58 +0900

    수정: name, role header expose

--- dddev/backend/src/main/java/com/d103/dddev/config/SecurityConfig.java
+++ dddev/backend/src/main/java/com/d103/dddev/config/SecurityConfig.java
@@ -68,6 +68,8 @@ public class SecurityConfig {
 		corsConfiguration.setMaxAge(7200L);
 		corsConfiguration.addExposedHeader("Authorization");
 		corsConfiguration.addExposedHeader("Authorization-refresh");
+		corsConfiguration.addExposedHeader("name");
+		corsConfiguration.addExposedHeader("role");
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", corsConfiguration);
 		return source;
