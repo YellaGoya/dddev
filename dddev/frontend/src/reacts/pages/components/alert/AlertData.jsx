aae18fb4f272f336c7089adb65cc82d199e3eb81
최가연 <cn01803@naver.com>
2023-11-13 17:26:26 +0900

    수정: 사용자 구독 알림 내역 조회

--- dddev/frontend/src/reacts/pages/components/alert/AlertData.jsx
+++ dddev/frontend/src/reacts/pages/components/alert/AlertData.jsx
@@ -1,18 +1,28 @@
-/* eslint-disable prettier/prettier */
 // import eetch from 'eetch/eetch';
 import { useState, useEffect } from 'react';
-// import { useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 // import requestPermission from 'fcm/firebase-messaging.js';
 import { db } from 'fcm/firebaseConfig';
 // import eetch from 'eetch/eetch';
 
 const AlertData = () => {
-  // const user = useSelector((state) => state.user);
-  const [userAlertList, setUserAlertList] = useState([{}]); // 구독중인 알림 내역
+  const user = useSelector((state) => state.user);
+  const groundsMap = useSelector((state) => state.user.groundsMap);
+  const [userDocIdList, setUserDocIdList] = useState([]);
+  const [userAlertList, setUserAlertList] = useState([{}]);
+  const [allAlertList, setAllAlertList] = useState([{}]);
 
   useEffect(() => {
-    // user.githubId 로 받아와야 합니다
-    const alertUserDataCollection = db.collection('alertUserData').where('githubId', '==', 111165249);
+    console.log(userAlertList);
+  }, [userAlertList]);
+
+  useEffect(() => {
+    console.log(allAlertList);
+  }, [allAlertList]);
+
+  useEffect(() => {
+    console.log(user.accessToken);
+    const alertUserDataCollection = db.collection('alertUserData').where('githubId', '==', Number(user.githubId));
 
     // firestore 실시간 동기화, 문서 변경 발생 시 실행
     alertUserDataCollection.onSnapshot(
@@ -25,42 +35,135 @@ const AlertData = () => {
           arr.push(doc.data());
         });
 
+        setUserDocIdList(arr);
+
         // 전체 알림에서 id로 사용자가 받을 알림을 찾고
-        const AlertList = [];
-        arr.forEach(({ id, isRead, keyword }) => {
-          db.collection('webhookData')
-            .doc(id)
-            .get()
-            .then((res) => {
-              if (res.exists) {
-                const resData = res.data();
-                // 알림 읽었는지, 걸린 키워드 뭔지 추가해줌
-                resData.isRead = isRead;
-                resData.keyowrd = keyword;
-                AlertList.push(resData);
-                console.log('list', resData); // 화면에 출력하는 데이터
-              }
-            });
-          setUserAlertList(AlertList);
-        });
+        // const AlertList = [];
+        // arr.forEach(({ id, isRead, keyword }) => {
+        //   db.collection('webhookData')
+        //     .doc(id)
+        //     .get()
+        //     .then((res) => {
+        //       if (res.exists) {
+        //         const resData = res.data();
+        //         // 알림 읽었는지, 걸린 키워드 뭔지 추가해줌
+        //         resData.isRead = isRead;
+        //         resData.keyowrd = keyword;
+        //         AlertList.push(resData);
+        //         console.log('list', resData); // 화면에 출력하는 데이터
+        //       }
+        //     });
+        //   setUserAlertList(AlertList);
+        // });
+        getUserDocs(arr);
       },
       (err) => {
         console.log(`snapshot error: ${err}`);
-      }
+      },
     );
-  }, []);
 
+    // 전체 웹훅 발생 문서 조회 -> 최초 1번만
+    // db.collection('webhookData').where()
+    getAllDocs(groundsMap);
+  }, [user]);
+
+  const getAllDocs = async (grounds) => {
+    const promises = grounds.map(({ id }) => {
+      console.log('id', id);
+      return db
+        .collection('webhookData')
+        .where('groundId', '==', `${id}`)
+        .get()
+        .then((res) => {
+          if (res.exists) {
+            const resData = res.data();
+            resData.nickname = resData.author.nickname;
+            console.log('resData ::', resData);
+            return resData;
+          }
+
+          return null;
+        });
+    });
+
+    const results = await Promise.all(promises);
+    const validResults = results.filter((result) => result !== null);
+    setAllAlertList(validResults);
+  };
+
+  const getUserDocs = async (arr) => {
+    const promises = arr.map(({ id, isRead, keyword }) => {
+      return db
+        .collection('webhookData')
+        .doc(id)
+        .get()
+        .then((res) => {
+          if (res.exists) {
+            const resData = res.data();
+            resData.isRead = isRead;
+            resData.keyword = keyword;
+            resData.nickname = resData.author.nickname;
+            return resData;
+          }
+
+          return null;
+        });
+    });
+
+    const results = await Promise.all(promises);
+    const validResults = results.filter((result) => result !== null);
+    setUserAlertList(validResults);
+  };
+
+  // const getUserDocs = (arr) => {
+  //   const AlertList = [];
+  //   // for(const [id, isRead, keyword] of arr) {}
+  //   arr.map(({ id, isRead, keyword }) => {
+  //     db.collection('webhookData')
+  //       .doc(id)
+  //       .get()
+  //       .then((res) => {
+  //         if (res.exists) {
+  //           const resData = res.data();
+  //           // 알림 읽었는지, 걸린 키워드 뭔지 추가해줌
+  //           resData.isRead = isRead;
+  //           resData.keyowrd = keyword;
+  //           AlertList.push(resData);
+  //           console.log('list', resData); // 화면에 출력하는 데이터
+  //         }
+  //       })
+  //       .catch((err) => console.log(err));
+  //     setUserAlertList(AlertList);
+  //   });
+
+  //   // await Promise.all(promises);
+  // };
+
+  // {allAlertList.map((doc) => {
+  //   return (
+  //     <div key={doc.id}>
+  //       <div>
+  //         {doc.type}
+  //         {/* {doc.author.nickname} */}
+  //         {doc.branch}
+  //         {doc.message}
+  //         {doc.timestamp}
+  //         {doc.url}
+  //       </div>
+  //     </div>
+  //   );
+  // })}
   return (
     <>
+      <div>사용자 알림 내역</div>
       {userAlertList.map((alert) => {
+        // console.log('html map', alert);
         return (
-          // alert.id 지금 중복이 있어서 수정할 계획 -> 원래 고유한 값 맞음
-          // 콘솔에는 나오는데 화면에는 뜨지 않는다... 왜지??ㅠㅠㅠㅠㅠㅠㅠ
-          // 저장할 때 잠깐 떴다가 사라지는거같은데?
-          <div key={alert}>
+          <div key={alert.id}>
             <div>
+              {alert.id}
               {alert.alertType}
-              {/* {alert.author.nickname} */}
+              {alert.author.nickname}
               {alert.branch}
               {alert.message}
               {alert.timestamp}
@@ -71,22 +174,8 @@ const AlertData = () => {
           </div>
         );
       })}
-      {/* <div>
-        {allDocs.map((doc) => {
-          return (
-            <div key={doc.id}>
-              <div>
-                {doc.type}
-                {doc.author.nickname}
-                {doc.branch}
-                {doc.message}
-                {doc.timestamp}
-                {doc.url}
-              </div>
-            </div>
-          );
-        })}
-      </div> */}
+      <div>전체 알림 내역</div>
+      {/* <div>{console.log('allAlertList :: ', allAlertList)}</div> */}
     </>
   );
 };
