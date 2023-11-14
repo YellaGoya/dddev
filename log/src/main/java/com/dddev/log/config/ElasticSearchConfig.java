a63d4d3c2f762f5a660fd55649babb19df655957
성연석 <abcd9351@naver.com>
2023-11-14 23:43:18 +0900

    Feat: Elastic 보안 설정

--- log/src/main/java/com/dddev/log/config/ElasticSearchConfig.java
+++ log/src/main/java/com/dddev/log/config/ElasticSearchConfig.java
@@ -1,21 +1,28 @@
 package com.dddev.log.config;
 
 import com.dddev.log.dto.ElasticSearchLog;
+import lombok.RequiredArgsConstructor;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.RestClients;
 import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @Configuration
+@RequiredArgsConstructor
 @EnableElasticsearchRepositories(basePackageClasses = {ElasticSearchLog.class})
 public class ElasticSearchConfig extends AbstractElasticsearchConfiguration {
 
+    private final Environment env;
+
     @Override
     public RestHighLevelClient elasticsearchClient() {
+
         ClientConfiguration clientConfiguration = ClientConfiguration.builder()
                 .connectedTo("k9d103a.p.ssafy.io:9200")
+                .withBasicAuth(env.getProperty("elasticSearch.id"), env.getProperty("elasticSearch.password"))
                 .build();
         return RestClients.create(clientConfiguration).rest();
     }
